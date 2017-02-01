09c10c69f16a219ced7927b0028b256e92d8f3dc
coutvv <coutvv@gmail.com>
2017-02-01 20:44:51 +0300

    init

---
+++ src/main/java/ru/coutvv/j8f/Main.java
@@ -0,0 +1,16 @@
+package ru.coutvv.j8f;
+
+import ru.coutvv.j8f.defaultmeth.MyDefaultMethods;
+
+/**
+ * Main class
+ *
+ * Created by coutvv on 01.02.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
