678859ba4ca8eb80bdaccf8dcfcfa8e11e95285b
coutvv <coutvv@gmail.com>
2017-02-17 16:46:06 +0300

    ch02threadSafe пример нарушения атомарности операции

--- src/main/java/ru/coutvv/concurrency/fundamental/threadsafety/playground/Atomicity.java
+++ src/main/java/ru/coutvv/concurrency/fundamental/threadsafety/playground/Atomicity.java
@@ -1,6 +1,13 @@
 package ru.coutvv.concurrency.fundamental.threadsafety.playground;
 
 /**
+ * Здесь показывается нарушение атомарности операции
+ * То есть первый поток берёт 6 и не успевает выполнить
+ * операцию, в то время как второй поток берёт эту же переменную
+ * и прибавляет к ней своё время. В момент когда первый поток добавляет
+ * своё время, он добавляет не к 6 а к результату из второго потока
+ * хотя по логике программы должен добавлять к 6.
+ *
  * Created by lomovtsevrs on 17.02.2017.
  */
 public class Atomicity {
@@ -12,15 +19,19 @@ public class Atomicity {
         return this.time;
     }
 
+    public static void lag() {
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         final Atomicity atom = new Atomicity();
         Thread th1 = new Thread(() -> {
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
             long before = atom.getTime();
+            lag();
             long after = atom.addTime(12);
             System.out.println("ко времени = " + before + " добавили 12 получили: " + after + " из потока th1");
         });
